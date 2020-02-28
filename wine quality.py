426d536eae2b88ef77eeed6c61c79c4b63250a94
ganeshbhrathwaj <40424499+ganeshbhrathwaj@users.noreply.github.com>
2020-02-28 22:02:26 +0530

    Update wine quality.py

--- wine quality.py
+++ wine quality.py
@@ -6,28 +6,19 @@ x=ds.iloc[:,0:11].values
 y=ds.iloc[:,-1].values
 
 
-
+#prediction with linear regression
 from sklearn.linear_model  import LinearRegression
 r=LinearRegression()
 r.fit(x,y)
 
 ypred=r.predict(x)
 
-
+#prediction with polynomial regression
 from sklearn.preprocessing import PolynomialFeatures
 preg=PolynomialFeatures(degree=4)
 xpoly=preg.fit_transform(x)
 reg1=LinearRegression()
 reg1.fit(xpoly,y)
-
 ypred1=reg1.predict(xpoly)
-a=0
-a1=0
-for i in range(0,1599):
-    a+=abs(y[i]-ypred[i])
-a=a/1599
 
-for i in range(0,1599):
-    a1+=abs(y[i]-ypred1[i])
-a1=a1/1599
     
